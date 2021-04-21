97542958f97747634c53478fb1ca8f95e6b1b7d9
Marco González <marcoignacio.9637@gmail.com>
2021-04-20 20:50:28 -0400

    style: Quite etiquetas y estilos

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,12 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ivideo
-      </h1>
-      <b-button variant="primary">BOTON DE PRUEBA</b-button>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="title">
+        HOME
+    </h1>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family:
     'Quicksand',
@@ -53,22 +18,10 @@ export default {}
     'Helvetica Neue',
     Arial,
     sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
 }
 </style>
