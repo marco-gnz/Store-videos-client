c86fd243477525aa02fac39bd22b4e1ddbca2d11
Marco González <marcoignacio.9637@gmail.com>
2021-04-24 19:41:47 -0400

    fix:Subiendo y cancelando archivos en el cliente a s3

--- components/Files/AppUploader.vue
+++ components/Files/AppUploader.vue
@@ -15,8 +15,55 @@ export default {
         Filepond.registerPlugin(FilePondPluginFileValidateType);
         Filepond.setOptions(es_Es);
         const pond = Filepond.create(this.$refs.file, {
-            acceptedFileTypes: supportedFiles
+            acceptedFileTypes: supportedFiles,
+            allowRevert: false,
+            onaddfile: (error, file) => {
+                if(error){
+                        return;
+                }
+
+                file.setMetadata("fileInfo", {
+                    name: file.filenameWithoutExtension,
+                    extension: file.fileExtension,
+                    size: file.fileSize
+                });
+            },
+            server: {
+                process: (fieldName, file, metadata, load, error, progress, abort) => {
+                    let formData = new FormData();
+                    const source = this.$axios.CancelToken.source();
+                    
+                        this.$axios.$post('/api/files/signed', {
+                            name: metadata.fileInfo.name,
+                            extension: metadata.fileInfo.extension,
+                            size: metadata.fileInfo.size
+                        }).then(response => {
+                        file.additionalData = response.additionalData;
+                        
+                        for(let field in file.additionalData){
+                            formData.append(field, file.additionalData[field]);
+                        }
+                        
+                        formData.append("file", file);
+                        this.$axios.$post(response.attributes.action, formData, {
+                            onUploadProgress(e) {
+                                progress(e.lengthComputable, e.loaded, e.total);
+                            },
+                            cancelToken: source.token
+                        }).then(_ => load(`${file.additionalData.key}`));
+                        });
+
+                        return {
+                            abort: () => {
+                                source.cancel();
+                                abort();
+                            }
+                        }
+                } 
+            }
         });
+
+
     }
 }
 </script>
