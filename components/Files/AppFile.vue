70ec9a142f714b07bb5da50ea2cc4a402f95838c
Marco González <marcoignacio.9637@gmail.com>
2021-04-26 14:19:41 -0400

    feat:Función para eliminar video

--- components/Files/AppFile.vue
+++ components/Files/AppFile.vue
@@ -4,18 +4,28 @@
             <span class="text-truncate">{{file.name}}</span>
             <div class="ml-auto pl-2">
                 <b-button variant="light" size="sm">Compartir</b-button>
-                <b-button variant="danger" size="sm">Eliminar</b-button>
+                <b-button variant="danger" size="sm" @click.prevent="deleteFile">Eliminar</b-button>
             </div>
         </b-list-group-item>
     </div>
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
     props: {
         file: {
             required: true,
             type: Object
+        }
+    },
+    methods: {
+        ...mapActions({
+            deleteFileAction: 'files/deleteFile'
+        }),
+        
+         deleteFile() {
+            this.deleteFileAction(this.file.uuid);
         }
     }
 }
--- store/files.js
+++ store/files.js
@@ -12,7 +12,11 @@ export const mutations = {
     },
 
     ADD_FILE(state, file){
-        state.files = [file, ...state.files]
+        state.files = [file, ...state.files];
+    },
+
+    REMOVE_FILE(state, uuid){
+        state.files = state.files.filter(file => file.uuid !== uuid);
     }
 };
 
@@ -30,6 +34,12 @@ export const actions = {
         const response = await this.$axios.$get('/api/files');
 
         commit('SET_FILES', response.data);
+    },
+
+    async deleteFile({ commit}, uuid) {
+        await this.$axios.$delete(`/api/file/${uuid}`);
+
+        commit('REMOVE_FILE', uuid);
     }
 
 };
